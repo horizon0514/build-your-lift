f28d8e33081cfe805fc96199161e12f240a49c3d
horizon0514 <horizon0514@gmail.com>
2024-03-10 10:56:23 +0800

    add gltf

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { Canvas } from '@react-three/fiber'
-
+import { CameraControls, Gltf } from '@react-three/drei'
 export default function App() {
   return (
     <div id="canvas-container">
@@ -9,14 +9,15 @@ export default function App() {
           left: 0,
           width: '100vw',
           height: '100vh',
-          pointerEvents: 'none',
         }}>
+        <CameraControls />
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
         <mesh>
           <boxGeometry args={[2, 2, 2]} />
           <meshStandardMaterial />
         </mesh>
+        <Gltf src="./COP_-_AKC_BES.gltf" />
       </Canvas>
     </div>
   )
